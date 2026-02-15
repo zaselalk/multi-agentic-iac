963deb218017e3b26549af8a703c97ced0b54fe7
Asela Priyadarshana <43272744+zaselalk@users.noreply.github.com>
2026-02-15 16:36:41 +0000

    Enhance post-create script to install AWS CLI and configure LocalStack credentials

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,6 +6,29 @@ echo "--- Installing OPA v0.46.0 ---"
 sudo curl -L -o /usr/local/bin/opa https://openpolicyagent.org/downloads/v0.46.0/opa_linux_amd64_static
 sudo chmod 755 /usr/local/bin/opa
 
+# --- 2. Install AWS CLI (Manual Install) ---
+echo "--- Installing AWS CLI ---"
+# Download and install v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install --update
+rm -rf aws awscliv2.zip
+
+# --- 3. Configure Dummy AWS Credentials ---
+echo "--- Configuring LocalStack Credentials ---"
+# Create the directory
+mkdir -p ~/.aws
+
+# Write the credentials file
+echo "[default]
+aws_access_key_id = test
+aws_secret_access_key = test" > ~/.aws/credentials
+
+# Write the config file
+echo "[default]
+region = us-east-1
+output = json" > ~/.aws/config
+
 #  Install LocalStack 
 echo "--- Installing LocalStack ---"
 /opt/conda/bin/pip install localstack
